c5ab9968a43e5b813efb7e4421ea0684dd6137cd
cedcass <cassignac.cedric@orange.fr>
2019-12-05 19:42:32 +0100

    first commit

---
+++ src/main/java/mgb/mcompetitions/McompetitionsApplication.java
@@ -0,0 +1,11 @@
+package mgb.mcompetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McompetitionsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(McompetitionsApplication.class, args);
+    }
+}
