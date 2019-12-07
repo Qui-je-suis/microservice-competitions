a51a500e2cf385320ac71cff6acdfdabc459c091
cedcass <cassignac.cedric@orange.fr>
2019-12-07 20:25:39 +0100

    add code formatter

--- src/main/java/mgb/mcompetitions/McompetitionsApplication.java
+++ src/main/java/mgb/mcompetitions/McompetitionsApplication.java
@@ -1,7 +1,7 @@
 /**
- *-------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
  * Copyright (C) 2019 QUI JE SUIS !!!. All rights reserved
- *-------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
  */
 package mgb.mcompetitions;
 
@@ -9,8 +9,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class McompetitionsApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(McompetitionsApplication.class, args);
-    }
+public class McompetitionsApplication
+{
+  public static void main(String[] args)
+  {
+    SpringApplication.run(McompetitionsApplication.class, args);
+  }
 }
